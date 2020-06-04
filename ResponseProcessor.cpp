9d4df39f8eb36309bc4557462769fe92ee697154
gerardd33 <gerardd@interia.pl>
2020-06-04 04:18:03 +0200

    Implemented ResponseProcessor

--- ResponseProcessor.cpp
+++ ResponseProcessor.cpp
@@ -8,5 +8,70 @@ bool ResponseProcessor::readStatusLine() {
 	fscanf(socketFile, "%m[^ ] %d %m[^\r\n]\n",
 		&protocoleVersion, &statusCode, &statusMessage);
 
-	if ()
+	free(protocoleVersion);
+	if (statusCode != 200 || strcmp(statusMessage, "OK") != 0) {
+		fprintf(stderr, "%d %s\n", statusCode, statusMessage);
+		free(statusMessage);
+		return false;
+	}
+
+	free(statusMessage);
+	return true;
+}
+
+void ResponseProcessor::readHeaders() {
+	char* line = nullptr;
+	char* parsedHeader = nullptr;
+	size_t bufferSize = 0;
+
+	while (true) {
+		if (getline(&line, &bufferSize, socketFile) == -1) {
+			ErrorHandler::syserr("getline");
+		}
+
+		if (strcmp(line, "\r\n") == 0) {
+			break;
+		}
+
+		convertHeaderNameToLowercase(line);
+		checkIfMetadataInterval(line);
+	}
+
+	free(line);
+	free(parsedHeader);
+
+	if (this->metadataInterval == -1) {
+		ErrorHandler::fatal("Processing server response");
+	}
+}
+
+void ResponseProcessor::readData() {
+	// TODO demultipleksowanie metadanych
+	std::vector<char> buffer(this->metadataInterval, 0);
+
+	size_t resourceLength = 0;
+
+	// TODO dokoncz, w szczegolnosci uwazaj na metadane i rozmiary blokow
+}
+
+void ResponseProcessor::convertHeaderNameToLowercase(char* line) {
+	for (size_t index = 0; line[index] != 0 && line[index] != ':'; ++index) {
+		line[index] = static_cast<char>(tolower(line[index]));
+	}
+}
+
+void ResponseProcessor::checkIfMetadataInterval(char* line) {
+	int readValue;
+	int sscanfResult = sscanf(line, "icy-metaint:[ ]%d", &readValue);
+	if (sscanfResult > 0) {
+		this->metadataInterval = readValue;
+	}
+}
+
+void ResponseProcessor::processServerResponse() {
+	if (!readStatusLine()) {
+		return;
+	}
+	readHeaders();
+	readData();
 }
--- ResponseProcessor.hpp
+++ ResponseProcessor.hpp
@@ -2,15 +2,23 @@
 #define RESPONSEPROCESSOR_HPP_
 
 #include "ErrorHandler.hpp"
+#include <cctype>
+#include <vector>
 
 class ResponseProcessor {
 public:
 	explicit ResponseProcessor(FILE* socketFile) : socketFile(socketFile) {};
+	void processServerResponse();
 
 private:
 	FILE* socketFile;
+	int metadataInterval = -1;
 
 	bool readStatusLine();
+	void readHeaders();
+	void readData();
+	void checkIfMetadataInterval(char* line);
+	void convertHeaderNameToLowercase(char* line);
 };
 
 #endif //RESPONSEPROCESSOR_HPP_
